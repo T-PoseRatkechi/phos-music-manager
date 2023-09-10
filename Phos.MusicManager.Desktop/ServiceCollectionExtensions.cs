386a5434b8ccbde943d12868ebb25733139aa82b
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 06:22:24 -0500

    Added dashboard view.

--- Phos.MusicManager.Desktop/ServiceCollectionExtensions.cs
+++ Phos.MusicManager.Desktop/ServiceCollectionExtensions.cs
@@ -2,12 +2,14 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Phos.MusicManager.Desktop.Library.ViewModels;
+using Phos.MusicManager.Library.ViewModels;
 
 internal static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddViewModels(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<MainWindowViewModel>();
+        serviceCollection.AddSingleton(s => new MainWindowViewModel(s.GetRequiredService<DashboardViewModel>()));
+        serviceCollection.AddSingleton<DashboardViewModel>();
         return serviceCollection;
     }
 }
