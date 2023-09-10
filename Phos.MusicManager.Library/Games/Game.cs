09cb1ac6ad67fce3dc585143c1cb2377a448acf1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 11:42:51 -0500

    Added game.

---
+++ Phos.MusicManager.Library/Games/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace Phos.MusicManager.Library.Games;
+
+/// <summary>
+/// Game instance.
+/// </summary>
+public class Game
+{
+    /// <summary>
+    /// Gets or sets the game name.
+    /// </summary>
+    public string Name { get; set; } = "Unknown Game";
+
+    /// <summary>
+    /// Gets or sets the output directory.
+    /// </summary>
+    public string? OutputDir { get; set; }
+}
