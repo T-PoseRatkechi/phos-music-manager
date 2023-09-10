8cb27e51f192c8ee8276dc65ba2b09f6be9d726d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 11:22:44 -0500

    Added generic savable file.

---
+++ Phos.MusicManager.Library/Common/ISavable.cs
@@ -0,0 +1,18 @@
+﻿namespace Phos.MusicManager.Library.Common;
+
+/// <summary>
+/// Savable object interface.
+/// </summary>
+/// <typeparam name="TValue">Value type.</typeparam>
+public interface ISavable<TValue>
+{
+    /// <summary>
+    /// Gets or sets current value.
+    /// </summary>
+    TValue Value { get; set; }
+
+    /// <summary>
+    /// Saves current value.
+    /// </summary>
+    void Save();
+}
---
+++ Phos.MusicManager.Library/Common/SavableFile.cs
@@ -0,0 +1,50 @@
+﻿namespace Phos.MusicManager.Library.Common;
+
+using Phos.MusicManager.Library.Common.Serializers;
+
+/// <summary>
+/// Savable file.
+/// </summary>
+/// <typeparam name="TValue">Value type.</typeparam>
+public class SavableFile<TValue> : ISavable<TValue>
+{
+    private readonly string filePath;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SavableFile{TValue}"/> class.
+    /// </summary>
+    /// <param name="filePath">File path.</param>
+    public SavableFile(string filePath)
+    {
+        this.filePath = filePath;
+        this.Value = this.GetCurrentValue();
+    }
+
+    /// <inheritdoc/>
+    public TValue Value { get; set; }
+
+    /// <inheritdoc/>
+    public void Save()
+    {
+        JsonFileSerializer.Serialize(this.filePath, this.Value);
+    }
+
+    private TValue GetCurrentValue()
+    {
+        if (!File.Exists(this.filePath))
+        {
+            return this.CreateDefault();
+        }
+
+        var currentValue = JsonFileSerializer.Deserialize<TValue>(this.filePath);
+        return currentValue ?? throw new ArgumentException($"Failed to load file.\nFile: {this.filePath}");
+    }
+
+    private TValue CreateDefault()
+    {
+        var defaultValue = Activator.CreateInstance<TValue>();
+        JsonFileSerializer.Serialize(this.filePath, defaultValue);
+
+        return defaultValue;
+    }
+}
