5c6c77ae57eee64c060697ec58a713f35f9a3391
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 10:19:41 -0500

    Added game constants.

---
+++ Phos.MusicManager.Library/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Phos.MusicManager.Library;
+
+#pragma warning disable SA1310 // Field names should not contain underscore
+#pragma warning disable SA1600 // Elements should be documented
+public static class Constants
+{
+    public const string P4G_PC_32 = "Persona 4 Golden PC x32";
+    public const string P4G_PC_64 = "Persona 4 Golden PC x64";
+    public const string P3P_PC = "Persona 3 Portable PC";
+    public const string P5R_PC = "Persona 5 Royal PC";
+    public const string P4 = "Persona 4";
+    public const string P3 = "Persona 3";
+}
