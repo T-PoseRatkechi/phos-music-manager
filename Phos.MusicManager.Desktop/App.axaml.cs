d3f58fb4a208d5f651090a605c5a80ea2219a3b7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 05:59:53 -0500

    Added DI.

--- Phos.MusicManager.Desktop/App.axaml.cs
+++ Phos.MusicManager.Desktop/App.axaml.cs
@@ -3,32 +3,46 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 using Phos.MusicManager.Desktop.Library.ViewModels;
 using Phos.MusicManager.Desktop.Views;
+using System;
 
-namespace Phos.MusicManager.Desktop
+namespace Phos.MusicManager.Desktop;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    private readonly IServiceProvider serviceProvider;
+
+    public App()
     {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+        var services = new ServiceCollection();
 
-        public override void OnFrameworkInitializationCompleted()
+        services.AddViewModels();
+
+        this.serviceProvider = services.BuildServiceProvider();
+    }
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            // Line below is needed to remove Avalonia data validation.
+            // Without this line you will get duplicate validations from both Avalonia and CT
+            BindingPlugins.DataValidators.RemoveAt(0);
+
+            var mainWindowVm = this.serviceProvider.GetRequiredService<MainWindowViewModel>();
+            desktop.MainWindow = new MainWindow
             {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
-            }
-
-            base.OnFrameworkInitializationCompleted();
+                DataContext = mainWindowVm,
+            };
         }
+
+        base.OnFrameworkInitializationCompleted();
     }
 }
---
+++ Phos.MusicManager.Desktop/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Phos.MusicManager.Desktop;
+
+using Microsoft.Extensions.DependencyInjection;
+using Phos.MusicManager.Desktop.Library.ViewModels;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddViewModels(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<MainWindowViewModel>();
+        return serviceCollection;
+    }
+}
