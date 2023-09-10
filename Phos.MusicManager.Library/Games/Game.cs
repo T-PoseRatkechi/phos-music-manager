1081c5b672ff96a3ae31b0399ada3ca5d1e50246
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 12:59:44 -0500

    Adjusted game and settings.

--- Phos.MusicManager.Library/Games/Game.cs
+++ Phos.MusicManager.Library/Games/Game.cs
@@ -1,17 +1,18 @@
 ﻿namespace Phos.MusicManager.Library.Games;
 
-/// <summary>
-/// Game instance.
-/// </summary>
+using Phos.MusicManager.Library.Audio;
+using Phos.MusicManager.Library.Common;
+
+#pragma warning disable SA1600 // Elements should be documented
 public class Game
 {
-    /// <summary>
-    /// Gets or sets the game name.
-    /// </summary>
-    public string Name { get; set; } = "Unknown Game";
+    public Game(string name, ISavable<GameSettings> settings)
+    {
+        this.Name = name;
+        this.Settings = settings;
+    }
 
-    /// <summary>
-    /// Gets or sets the output directory.
-    /// </summary>
-    public string? OutputDir { get; set; }
+    public string Name { get; set; }
+
+    public ISavable<GameSettings> Settings { get; set; }
 }
---
+++ Phos.MusicManager.Library/Games/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Phos.MusicManager.Library.Games;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class GameSettings
+{
+    public string Theme { get; set; } = "Dark";
+
+    public string? InstallPath { get; set; }
+
+    public string? OutputDir { get; set; }
+}
