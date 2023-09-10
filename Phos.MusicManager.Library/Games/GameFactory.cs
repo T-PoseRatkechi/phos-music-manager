07b8a670ef17a3a67259edfaf14e3ed5c5f6dbf9
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 13:03:05 -0500

    Added game factory.

---
+++ Phos.MusicManager.Library/Games/GameFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace Phos.MusicManager.Library.Games;
+
+using Phos.MusicManager.Library.Common;
+
+/// <summary>
+/// Game factory.
+/// </summary>
+public class GameFactory : IGameFactory
+{
+    private readonly string gamesDir = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "games");
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameFactory"/> class.
+    /// </summary>
+    public GameFactory()
+    {
+        Directory.CreateDirectory(this.gamesDir);
+    }
+
+    /// <inheritdoc/>
+    public Game GetGame(string name)
+    {
+        var gameFolder = Path.Join(this.gamesDir, name);
+        Directory.CreateDirectory(gameFolder);
+
+        var settingsFile = Path.Join(gameFolder, "game-settings.json");
+        var settings = new SavableFile<GameSettings>(settingsFile);
+        var game = new Game(name, settings);
+        return game;
+    }
+}
---
+++ Phos.MusicManager.Library/Games/IGameFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Phos.MusicManager.Library.Games;
+
+/// <summary>
+/// Game factory interface.
+/// </summary>
+public interface IGameFactory
+{
+    /// <summary>
+    /// Gets an existing game instance or creates a new one.
+    /// </summary>
+    /// <param name="name">Name of game.</param>
+    /// <returns>Game instance.</returns>
+    Game GetGame(string name);
+}
