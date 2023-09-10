94f7ea4e9346f01adb8b2750301e209290990877
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-10 11:21:00 -0500

    Added JSON serializer.

---
+++ Phos.MusicManager.Library/Serializers/JsonFileSerializer.cs
@@ -0,0 +1,20 @@
+﻿namespace Phos.MusicManager.Library.Common.Serializers;
+
+using System.Text.Json;
+
+#pragma warning disable SA1600 // Elements should be documented
+public static class JsonFileSerializer
+{
+    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
+
+    public static void Serialize(string file, object? value)
+    {
+        File.WriteAllText(file, JsonSerializer.Serialize(value, Options));
+    }
+
+    public static T? Deserialize<T>(string file)
+    {
+        var value = JsonSerializer.Deserialize<T>(File.ReadAllText(file));
+        return value;
+    }
+}
