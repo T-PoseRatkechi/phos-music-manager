07d61777385245387b754a6a5d5c0dc481d49787
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 12:03:36 -0500

    Added null converter.

---
+++ Phos.MusicManager.Desktop/Converters/NullBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+public class NullBoolConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is not null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is null;
+    }
+}
