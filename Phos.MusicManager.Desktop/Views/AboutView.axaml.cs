5154fdc604046ef25d0fb08737f19087f24fc2c7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 12:05:01 -0500

    Added about view.

---
+++ Phos.MusicManager.Desktop/Views/AboutView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Phos.MusicManager.Desktop.Views;
+
+public partial class AboutView : UserControl
+{
+    public AboutView()
+    {
+        InitializeComponent();
+    }
+}
