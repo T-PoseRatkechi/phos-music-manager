d2542269cc7e3a3349d146daa62861bb6627b77b
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 03:31:37 -0500

    Added page interface.

---
+++ Phos.MusicManager.Library/Navigation/IPage.cs
@@ -0,0 +1,12 @@
+﻿namespace Phos.MusicManager.Library.Navigation;
+
+/// <summary>
+/// Navigable page interface.
+/// </summary>
+public interface IPage
+{
+    /// <summary>
+    /// Gets navigation name.
+    /// </summary>
+    string Name { get; }
+}
