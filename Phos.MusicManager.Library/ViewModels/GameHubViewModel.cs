19a01a180b41b9df811709b30f377b31b0a0d01e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 09:16:22 -0500

    Added game hub vm.

---
+++ Phos.MusicManager.Library/ViewModels/GameHubViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Phos.MusicManager.Library.ViewModels;
+
+using Phos.MusicManager.Desktop.Library.ViewModels;
+using Phos.MusicManager.Library.Games;
+using Phos.MusicManager.Library.Navigation;
+
+/// <summary>
+/// Game hub view model.
+/// </summary>
+public class GameHubViewModel : ViewModelBase, IPage
+{
+    private readonly Game game;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameHubViewModel"/> class.
+    /// </summary>
+    /// <param name="game"></param>
+    public GameHubViewModel(Game game)
+    {
+        this.game = game;
+        this.Name = game.Name;
+    }
+
+    /// <inheritdoc/>
+    public string Name { get; }
+}
