30432daf2f4e6b41e12e1f7ccedd14842216e6c1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 09:13:58 -0500

    Added settings and about page vms.

---
+++ Phos.MusicManager.Library/ViewModels/AboutViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Phos.MusicManager.Library.ViewModels;
+
+using Phos.MusicManager.Desktop.Library.ViewModels;
+using Phos.MusicManager.Library.Navigation;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class AboutViewModel : ViewModelBase, IPage
+{
+    public string Name { get; } = "About";
+
+    public Credit[] Credits { get; } = new Credit[]
+    {
+        new("T-PoseRatkechi", "Creator"),
+        new("Pixelguin", "Provided Persona 4 Golden 64-bit music data and song categories.", "https://gamebanana.com/members/1736439"),
+        new("aray03", "Source for Persona 5 Royal PC music data.", "https://gamebanana.com/members/2323791"),
+        new("ARandomGuy231 + Various Modders", "Source for Persona 3 Portable music data.", "https://gamebanana.com/members/1742788"),
+        new("Thealexbarney", "VGAudio library, used to encode ADX and HCA.", "https://github.com/Thealexbarney/VGAudio"),
+    };
+
+    public record Credit(string Name, string Description, string? Url = null);
+}
---
+++ Phos.MusicManager.Library/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Phos.MusicManager.Library.ViewModels;
+
+using Phos.MusicManager.Desktop.Library.ViewModels;
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.Games;
+using Phos.MusicManager.Library.Navigation;
+
+/// <summary>
+/// Settings view model.
+/// </summary>
+public class SettingsViewModel : ViewModelBase, IPage
+{
+    private readonly ISavable<AppSettings> settings;
+    private readonly IGameService gameService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SettingsViewModel"/> class.
+    /// </summary>
+    /// <param name="settings"></param>
+    /// <param name="gameService"></param>
+    public SettingsViewModel(ISavable<AppSettings> settings, IGameService gameService)
+    {
+        this.settings = settings;
+        this.gameService = gameService;
+    }
+
+    /// <inheritdoc/>
+    public string Name { get; } = "Settings";
+
+    /// <summary>
+    /// Gets or sets a value indicating whether debug mode is enabled.
+    /// </summary>
+    public bool DebugEnabled
+    {
+        get => this.settings.Value.DebugEnabled;
+        set
+        {
+            this.settings.Value.DebugEnabled = value;
+            this.settings.Save();
+        }
+    }
+}
