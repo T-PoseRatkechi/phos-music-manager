663896f59d1b9c714e754e247760831f3934b3d8
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 14:28:02 -0500

    Reworked default music provider to return path to local default music file.

---
+++ Phos.MusicManager.Library/Audio/DefaultMusic.cs
@@ -0,0 +1,20 @@
+﻿namespace Phos.MusicManager.Library.Audio;
+
+/// <summary>
+/// Default music provider.
+/// </summary>
+public static class DefaultMusic
+{
+    private static readonly string DefaultMusicDir = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Music");
+
+    /// <summary>
+    /// Gets the default music file for game.
+    /// </summary>
+    /// <param name="name">Game name.</param>
+    /// <returns>Path to default music file.</returns>
+    public static string? GetDefaultMusicFile(string name)
+    {
+        var gameMusicFile = Path.Join(DefaultMusicDir, $"{name}.json");
+        return File.Exists(gameMusicFile) ? gameMusicFile : null;
+    }
+}
