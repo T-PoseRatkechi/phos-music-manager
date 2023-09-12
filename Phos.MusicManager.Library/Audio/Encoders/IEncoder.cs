f83831914c0993f84527cf10bab32827198b4493
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 03:19:00 -0500

    Added audio encoders.

---
+++ Phos.MusicManager.Library/Audio/Encoders/IEncoder.cs
@@ -0,0 +1,28 @@
+﻿namespace Phos.MusicManager.Library.Audio.Encoders;
+
+using Phos.MusicManager.Library.Audio.Models;
+
+/// <summary>
+/// Encoder interface.
+/// </summary>
+public interface IEncoder
+{
+    /// <summary>
+    /// Gets default file extension of encoded file.
+    /// </summary>
+    string EncodedExt { get; }
+
+    /// <summary>
+    /// Gets list of valid input formats by extension.
+    /// </summary>
+    string[] InputFormats { get; }
+
+    /// <summary>
+    /// Encode file.
+    /// </summary>
+    /// <param name="inputFile">Input file.</param>
+    /// <param name="outputFile">Output file.</param>
+    /// <param name="loop">Loop settings.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task Encode(string inputFile, string outputFile, Loop? loop = null);
+}
