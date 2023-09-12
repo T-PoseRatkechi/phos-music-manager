57783e8f4820219d81b4f2ba045f08f4542da5fc
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 03:40:05 -0500

    Added checksum util.

---
+++ Phos.MusicManager.Library/Common/Checksum.cs
@@ -0,0 +1,15 @@
+﻿namespace Phos.MusicManager.Library.Common;
+
+using System.Security.Cryptography;
+
+#pragma warning disable SA1600 // Elements should be documented
+public static class Checksum
+{
+    public static string Compute(string inputFile)
+    {
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(inputFile);
+        var hash = md5.ComputeHash(stream);
+        return Convert.ToHexString(hash);
+    }
+}
