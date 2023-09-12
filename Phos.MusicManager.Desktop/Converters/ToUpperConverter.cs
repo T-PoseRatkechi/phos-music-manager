f3735d68cf8ccc4643e503c1c45384b66bd8f1e7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-11 19:05:15 -0500

    Added uppercase converter.

---
+++ Phos.MusicManager.Desktop/Converters/ToUpperConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+internal class ToUpperConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString()?.ToUpper();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
