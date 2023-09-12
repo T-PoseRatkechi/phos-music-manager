b603442590206e860ea9bbc74253e470d51d21da
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 09:30:28 -0500

    Added path name converter.

---
+++ Phos.MusicManager.Desktop/Converters/PathNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+public class PathNameConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string path)
+        {
+            try
+            {
+                return Path.GetFileName(path);
+            }
+            catch (Exception)
+            {
+                return path;
+            }
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
