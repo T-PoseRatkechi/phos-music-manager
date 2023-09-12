02089077462bb82357bbfa9773c96498d18d5641
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 05:19:50 -0500

    Added conversion for HCA quality.

---
+++ Phos.MusicManager.Library/Audio/Models/EncodeQualityExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Phos.MusicManager.Library.Audio.Models;
+
+using VGAudio.Codecs.CriHca;
+
+#pragma warning disable SA1600 // Elements should be documented
+public static class EncodeQualityExtensions
+{
+    public static CriHcaQuality ToHcaQuality(this EncodeQuality quality)
+    {
+        return quality switch
+        {
+            EncodeQuality.Highest => CriHcaQuality.Highest,
+            EncodeQuality.High => CriHcaQuality.High,
+            EncodeQuality.Medium => CriHcaQuality.Middle,
+            EncodeQuality.Low => CriHcaQuality.Low,
+            EncodeQuality.Lowest => CriHcaQuality.Lowest,
+            _ => CriHcaQuality.NotSet,
+        };
+    }
+}
