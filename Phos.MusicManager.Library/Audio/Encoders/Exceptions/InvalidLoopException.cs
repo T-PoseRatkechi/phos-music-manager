f63a1c2bf5435feebc8b8b6037e340c40fcd8c5e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 03:12:19 -0500

    Added loop exception.

---
+++ Phos.MusicManager.Library/Audio/Encoders/Exceptions/InvalidLoopException.cs
@@ -0,0 +1,17 @@
+﻿namespace Phos.MusicManager.Library.Audio.Encoders.Exceptions;
+
+using Phos.MusicManager.Library.Audio.Models;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class InvalidLoopException : Exception
+{
+    public InvalidLoopException(Loop loop)
+    {
+        this.Loop = loop;
+    }
+
+    public Loop Loop { get; }
+
+    public override string Message
+        => $"End sample must be greater than start sample.\nStart Sample: {this.Loop.StartSample}\nEnd Sample: {this.Loop.EndSample}";
+}
