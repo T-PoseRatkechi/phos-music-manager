ffc223992a5a42ef0d6dd1b1b10f018087c23e36
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 17:39:30 -0500

    Added track panel.

---
+++ Phos.MusicManager.Desktop/Views/Music/TrackPanelView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Phos.MusicManager.Desktop.Views.Music;
+
+public partial class TrackPanelView : UserControl
+{
+    public TrackPanelView()
+    {
+        InitializeComponent();
+    }
+}
