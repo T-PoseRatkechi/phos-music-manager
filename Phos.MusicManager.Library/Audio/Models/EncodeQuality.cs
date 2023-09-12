834bf63e9d1a7e5e21be15498d1263bb878c7a0d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 03:23:41 -0500

    Moved encode quality.

--- Phos.MusicManager.Library/Audio/Models/EncodeQuality.cs
+++ Phos.MusicManager.Library/Audio/Models/EncodeQuality.cs
@@ -1,4 +1,4 @@
-﻿namespace Phos.MusicManager.Library.Audio.Processors;
+﻿namespace Phos.MusicManager.Library.Audio.Models;
 
 #pragma warning disable SA1600 // Elements should be documented
 #pragma warning disable SA1602 // Enumeration items should be documented
--- Phos.MusicManager.Library/Audio/Processors/Encoder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Phos.MusicManager.Library.Audio.Processors;
-
-#pragma warning disable SA1600 // Elements should be documented
-public static class Encoder
-{
-    public const string HCA = "HCA";
-
-    public const string ADX = "ADX";
-}
