487378eebc87cbb01ed3e5ca00c5ceebce439834
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-12 10:18:08 -0500

    Added audio track control.

---
+++ Phos.MusicManager.Desktop/Controls/Track.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Phos.MusicManager.Desktop.Controls;
+
+public partial class Track : UserControl
+{
+    public Track()
+    {
+        InitializeComponent();
+    }
+}
