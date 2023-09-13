477185f49269ace79d7c58c21172464f4a7815ba
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-13 15:12:24 -0500

    Added audio builder.

---
+++ Phos.MusicManager.Library/Audio/AudioBuilder.cs
@@ -0,0 +1,65 @@
+﻿namespace Phos.MusicManager.Library.Audio;
+
+using Microsoft.Extensions.Logging;
+using Phos.MusicManager.Library.Audio.Encoders;
+using Phos.MusicManager.Library.Audio.Models;
+
+/// <summary>
+/// Audio builder.
+/// </summary>
+public class AudioBuilder
+{
+    private readonly ILogger? log;
+    private readonly AudioEncoderRegistry encoderRegistry;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AudioBuilder"/> class.
+    /// </summary>
+    /// <param name="encoderRegistry"></param>
+    /// <param name="log"></param>
+    public AudioBuilder(AudioEncoderRegistry encoderRegistry, ILogger? log = null)
+    {
+        this.log = log;
+        this.encoderRegistry = encoderRegistry;
+    }
+
+    /// <summary>
+    /// Builds the audio tracks.
+    /// </summary>
+    /// <param name="tracks">Tracks to build.</param>
+    /// <param name="outputDir">Path to output build.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task Build(IEnumerable<AudioTrack> tracks, string outputDir)
+    {
+        foreach (var track in tracks)
+        {
+            if (string.IsNullOrEmpty(track.OutputPath))
+            {
+                throw new ArgumentException($"Track output path is missing.\nTrack: {track.Name}");
+            }
+
+            var outputFile = Path.Join(outputDir, track.OutputPath);
+
+            // Delete previous output file.
+            if (File.Exists(outputFile))
+            {
+                File.Delete(outputFile);
+            }
+
+            if (track.ReplacementFile == null)
+            {
+                continue;
+            }
+
+            Directory.CreateDirectory(Path.GetDirectoryName(outputFile)!);
+            if (this.encoderRegistry.Encoders.TryGetValue(track.Encoder, out var encoder))
+            {
+                await encoder.Encode(track.ReplacementFile, outputFile, track.Loop);
+            }
+            else
+            {
+                this.log?.LogWarning("Unknown encoder {encoder} for track {track}.", track.Encoder, track.Name);
+            }
+        }
+    }
+}
