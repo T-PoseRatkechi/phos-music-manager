c14a47aa5c6c384c1f25a79ba855b29387dc3348
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-13 02:44:49 -0500

    Added custom window to connect window views and view models.

---
+++ Phos.MusicManager.Desktop/Controls/AppWindow.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using Phos.MusicManager.Library.ViewModels;
+using System;
+
+namespace Phos.MusicManager.Desktop.Controls;
+
+public class AppWindow : Window
+{
+    protected override void OnOpened(EventArgs e)
+    {
+        base.OnOpened(e);
+
+        if (this.DataContext is WindowViewModelBase viewModel)
+        {
+            viewModel.Closing += this.ViewModel_Closing;
+        }
+    }
+
+    private void ViewModel_Closing(object? sender, object? e)
+    {
+        if (this.DataContext is WindowViewModelBase viewModel)
+        {
+            viewModel.Closing -= this.ViewModel_Closing;
+        }
+
+        this.Close(e);
+    }
+}
---
+++ Phos.MusicManager.Library/ViewModels/WindowViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Phos.MusicManager.Library.ViewModels;
+
+using Phos.MusicManager.Desktop.Library.ViewModels;
+
+/// <summary>
+/// Window view model base.
+/// </summary>
+public class WindowViewModelBase : ViewModelBase
+{
+    /// <summary>
+    /// Window closing event.
+    /// </summary>
+    public event EventHandler<object?>? Closing;
+
+    /// <summary>
+    /// Close window.
+    /// </summary>
+    /// <param name="result">Dialog result.</param>
+    public void Close(object? result = null)
+    {
+        this.Closing?.Invoke(this, result);
+    }
+}
