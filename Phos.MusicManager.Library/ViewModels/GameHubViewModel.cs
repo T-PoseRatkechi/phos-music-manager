d162be28b62302ca13436027ec8f180a0a9415e1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-13 15:21:08 -0500

    Added build command.

--- Phos.MusicManager.Library/ViewModels/GameHubViewModel.cs
+++ Phos.MusicManager.Library/ViewModels/GameHubViewModel.cs
@@ -3,35 +3,39 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 using Phos.MusicManager.Desktop.Library.ViewModels;
+using Phos.MusicManager.Library.Audio;
 using Phos.MusicManager.Library.Audio.Models;
 using Phos.MusicManager.Library.Common;
 using Phos.MusicManager.Library.Games;
 using Phos.MusicManager.Library.Navigation;
 using Phos.MusicManager.Library.ViewModels.Music;
 
-/// <summary>
-/// Game hub view model.
-/// </summary>
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1601 // Partial elements should be documented
 public partial class GameHubViewModel : ViewModelBase, IPage
 {
     private readonly Game game;
+    private readonly AudioBuilder audioBuilder;
     private readonly MusicFactory musicFactory;
     private readonly IDialogService dialog;
+    private readonly ILogger? log;
 
     [ObservableProperty]
     private TrackPanelViewModel? trackPanel;
     private AudioTrack? selectedTrack;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="GameHubViewModel"/> class.
-    /// </summary>
-    /// <param name="game"></param>
-    /// <param name="musicFactory"></param>
-    /// <param name="dialog"></param>
-    public GameHubViewModel(Game game, MusicFactory musicFactory, IDialogService dialog)
+    public GameHubViewModel(
+        Game game,
+        AudioBuilder audioBuilder,
+        MusicFactory musicFactory,
+        IDialogService dialog,
+        ILogger? log = null)
     {
+        this.log = log;
         this.game = game;
+        this.audioBuilder = audioBuilder;
         this.musicFactory = musicFactory;
         this.dialog = dialog;
     }
@@ -39,14 +43,8 @@ public partial class GameHubViewModel : ViewModelBase, IPage
     /// <inheritdoc/>
     public string Name => this.game.Name;
 
-    /// <summary>
-    /// Gets game audio tracks.
-    /// </summary>
     public ObservableCollection<AudioTrack> Tracks => this.game.Audio.Tracks;
 
-    /// <summary>
-    /// Gets or sets selected track.
-    /// </summary>
     public AudioTrack? SelectedTrack
     {
         get => this.selectedTrack;
@@ -57,16 +55,25 @@ public partial class GameHubViewModel : ViewModelBase, IPage
         }
     }
 
-    private void UpdateTrackPanel()
-    {
-        this.TrackPanel?.Dispose();
-        if (this.SelectedTrack == null)
-        {
-            this.TrackPanel = null;
-            return;
-        }
+    private bool CanBuild { get; set; } = true;
 
-        this.TrackPanel = this.musicFactory.CreateTrackPanel(this.SelectedTrack, this.game.Audio, this.CloseTrackPanelCommand);
+    [RelayCommand(CanExecute = nameof(this.CanBuild))]
+    private async Task Build()
+    {
+        try
+        {
+            this.CanBuild = false;
+            var outputDir = this.game.Settings.Value.OutputDir ?? this.game.BuildFolder;
+            await this.audioBuilder.Build(this.game.Audio.Tracks, outputDir);
+        }
+        catch (Exception ex)
+        {
+            this.log?.LogError(ex, "Failed to build output for game {game}.", this.game.Name);
+        }
+        finally
+        {
+            this.CanBuild = true;
+        }
     }
 
     [RelayCommand]
@@ -84,5 +91,17 @@ public partial class GameHubViewModel : ViewModelBase, IPage
     private void CloseTrackPanel()
     {
         this.SelectedTrack = null;
+    }
+
+    private void UpdateTrackPanel()
+    {
+        this.TrackPanel?.Dispose();
+        if (this.SelectedTrack == null)
+        {
+            this.TrackPanel = null;
+            return;
+        }
+
+        this.TrackPanel = this.musicFactory.CreateTrackPanel(this.SelectedTrack, this.game.Audio, this.CloseTrackPanelCommand);
     }
 }
