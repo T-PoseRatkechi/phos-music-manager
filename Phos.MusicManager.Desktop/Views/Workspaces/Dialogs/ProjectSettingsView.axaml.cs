b4e7d14da6ac55b13f58be3038d8a76e85813141
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-14 15:25:25 -0500

    Added project settings view.

---
+++ Phos.MusicManager.Desktop/Views/Workspaces/Dialogs/ProjectSettingsView.axaml.cs
@@ -0,0 +1,11 @@
+using Phos.MusicManager.Desktop.Controls;
+
+namespace Phos.MusicManager.Desktop.Views.Workspaces.Dialogs;
+
+public partial class ProjectSettingsView : AppWindow
+{
+    public ProjectSettingsView()
+    {
+        InitializeComponent();
+    }
+}
