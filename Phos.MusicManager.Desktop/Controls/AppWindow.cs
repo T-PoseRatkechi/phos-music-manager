a09dd706754c80932a36eefb94803008034ebf7a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-14 06:35:13 -0500

    Fixed issue with closing from view.

--- Phos.MusicManager.Desktop/Controls/AppWindow.cs
+++ Phos.MusicManager.Desktop/Controls/AppWindow.cs
@@ -12,24 +12,42 @@ public class AppWindow : Window
 
         if (this.DataContext is WindowViewModelBase viewModel)
         {
+            // Connect to close from view.
+            this.Closing += View_Closing;
+
             // Connect to close from view model.
             viewModel.Closing += this.ViewModel_Closing;
-
-            // Connect to close from view.
-            this.Closing += this.ViewModel_Closing;
         }
     }
 
+    /// <summary>
+    /// Handles closing from view.
+    /// </summary>
+    private void View_Closing(object? sender, WindowClosingEventArgs e)
+    {
+        if (this.DataContext is WindowViewModelBase viewModel)
+        {
+            // Unregister event listeners.
+            this.Closing -= this.View_Closing;
+            viewModel.Closing -= this.ViewModel_Closing;
+
+            // Run vm close function.
+            viewModel.Close();
+        }
+
+        this.Close();
+    }
+
+    /// <summary>
+    /// Handles closing from view model.
+    /// </summary>
     private void ViewModel_Closing(object? sender, object? e)
     {
         if (this.DataContext is WindowViewModelBase viewModel)
         {
-            // Run close function from vm.
-            viewModel.Close();
-
             // Unregister event listeners.
+            this.Closing -= this.View_Closing;
             viewModel.Closing -= this.ViewModel_Closing;
-            this.Closing -= this.ViewModel_Closing;
         }
 
         this.Close(e);
