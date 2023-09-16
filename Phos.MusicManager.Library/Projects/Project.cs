53daf62701f755305b5178be8ebfdfe9a41f949e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-16 11:54:40 -0500

    Added project type and repo. Removed workspace types.

---
+++ Phos.MusicManager.Library/Projects/Project.cs
@@ -0,0 +1,34 @@
+﻿namespace Phos.MusicManager.Library.Projects;
+
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Phos.MusicManager.Library.Audio;
+using Phos.MusicManager.Library.Audio.Models;
+using Phos.MusicManager.Library.Common;
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1601 // Partial elements should be documented
+public partial class Project : ObservableObject
+{
+    public Project(string projectFile)
+    {
+        this.Settings = new SavableFile<ProjectSettings>(projectFile);
+        this.ProjectFolder = Directory.CreateDirectory(Path.GetDirectoryName(projectFile)!).FullName;
+        this.AudioFolder = Directory.CreateDirectory(Path.Join(this.ProjectFolder, "audio")).FullName;
+        this.BuildFolder = Directory.CreateDirectory(Path.Join(this.ProjectFolder, "build")).FullName;
+
+        var audioTracksFile = Path.Join(this.AudioFolder, "audio-tracks.json");
+        var audioTracks = new SavableFile<ObservableCollection<AudioTrack>>(audioTracksFile);
+        this.Audio = new(audioTracks);
+    }
+
+    public string ProjectFolder { get; }
+
+    public string AudioFolder { get; }
+
+    public string BuildFolder { get; }
+
+    public ISavable<ProjectSettings> Settings { get; }
+
+    public AudioService Audio { get; }
+}
---
+++ Phos.MusicManager.Library/Projects/ProjectRepository.cs
@@ -0,0 +1,135 @@
+﻿namespace Phos.MusicManager.Library.Projects;
+
+using System.Collections.ObjectModel;
+using Microsoft.Extensions.Logging;
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.Common.Serializers;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class ProjectRepository
+{
+    private readonly ILogger? log;
+    private readonly string projectsDir;
+    private readonly IRepository<ProjectPreset, string> presetRepo;
+    private readonly ISavable<AppSettings> appSettings;
+
+    public ProjectRepository(
+        IRepository<ProjectPreset, string> presetRepo,
+        ISavable<AppSettings> appSettings,
+        ILogger? log = null)
+    {
+        this.log = log;
+        this.presetRepo = presetRepo;
+        this.appSettings = appSettings;
+        this.projectsDir = Directory.CreateDirectory(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "projects")).FullName;
+
+        this.LoadProjects();
+        this.AddDefaultProjects();
+    }
+
+    public ObservableCollection<Project> List { get; } = new();
+
+    public Project Create(ProjectSettings settings)
+    {
+        var projectFile = Path.Join(this.projectsDir, settings.Name, $"project.phos");
+        if (File.Exists(projectFile))
+        {
+            throw new Exception("Project file already exists.");
+        }
+
+        JsonFileSerializer.Serialize(projectFile, settings);
+        this.appSettings.Value.ProjectFiles.Add(projectFile);
+        this.appSettings.Save();
+
+        var project = new Project(projectFile);
+        if (project.Settings.Value.Preset != null)
+        {
+            if (this.presetRepo.GetById(project.Settings.Value.Preset) is ProjectPreset preset)
+            {
+                project.Audio.AddTracks(preset.DefaultTracks);
+            }
+            else
+            {
+                throw new Exception($"Project preset not found.\nPreset: {project.Settings.Value.Preset}");
+            }
+        }
+
+        this.List.Add(project);
+        return project;
+    }
+
+    public void Delete(Project item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Project? GetById(string id)
+    {
+        return this.List.FirstOrDefault(x => x.Settings.Value.Name == id);
+    }
+
+    public bool Update(Project item)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void LoadProjects()
+    {
+        var missingProjectFiles = new List<string>();
+        foreach (var projectFile in this.appSettings.Value.ProjectFiles)
+        {
+            if (!File.Exists(projectFile))
+            {
+                missingProjectFiles.Add(projectFile);
+                continue;
+            }
+
+            try
+            {
+                var project = new Project(projectFile);
+                this.List.Add(project);
+            }
+            catch (Exception ex)
+            {
+                this.log?.LogError(ex, "Failed to load project file.\nFile: {file}", projectFile);
+            }
+        }
+
+        // Remove missing projects.
+        if (missingProjectFiles.Count > 0)
+        {
+            foreach (var file in missingProjectFiles)
+            {
+                this.appSettings.Value.ProjectFiles.Remove(file);
+            }
+
+            this.appSettings.Save();
+        }
+    }
+
+    private void AddDefaultProjects()
+    {
+        foreach (var preset in GamePresets.DefaultPresets)
+        {
+            if (this.GetById(preset.Name) == null)
+            {
+                var settings = new ProjectSettings
+                {
+                    Name = preset.Name,
+                    Color = preset.Color,
+                    Preset = preset.Name,
+                    PostBuild = preset.PostBuild,
+                };
+
+                try
+                {
+                    this.Create(settings);
+                }
+                catch (Exception ex)
+                {
+                    this.log?.LogError(ex, "Failed to create default project {name}.", preset.Name);
+                }
+            }
+        }
+    }
+}
