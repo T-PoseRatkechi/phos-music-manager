97a56149919667fbd488925c092c316345776aac
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-16 17:58:20 -0500

    Added create project view.

---
+++ Phos.MusicManager.Desktop/Views/Projects/Dialogs/CreateProjectView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Phos.MusicManager.Desktop.Controls;
+
+namespace Phos.MusicManager.Desktop.Views.Projects.Dialogs;
+
+public partial class CreateProjectView : AppWindow
+{
+    public CreateProjectView()
+    {
+        InitializeComponent();
+    }
+}
