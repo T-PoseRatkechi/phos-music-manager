046c1c69d844fe8d52adf9ff28f00d68c0180ce0
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-16 11:43:26 -0500

    Rename project settings view.

--- Phos.MusicManager.Desktop/Views/Projects/Dialogs/ProjectSettingsView.axaml.cs
+++ Phos.MusicManager.Desktop/Views/Projects/Dialogs/ProjectSettingsView.axaml.cs
@@ -1,6 +1,6 @@
 using Phos.MusicManager.Desktop.Controls;
 
-namespace Phos.MusicManager.Desktop.Views.Workspaces.Dialogs;
+namespace Phos.MusicManager.Desktop.Views.Projects.Dialogs;
 
 public partial class ProjectSettingsView : AppWindow
 {
