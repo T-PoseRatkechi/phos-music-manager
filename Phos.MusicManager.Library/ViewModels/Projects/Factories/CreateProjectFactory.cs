f454d8e17d2e140ddee9ac4b29eac67f9e4e2f0d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 06:31:57 -0500

    Added factories.

---
+++ Phos.MusicManager.Library/ViewModels/Projects/Factories/CreateProjectFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Phos.MusicManager.Library.ViewModels.Projects.Factories;
+
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.Projects;
+using Phos.MusicManager.Library.ViewModels.Projects.Dialogs;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class CreateProjectFactory
+{
+    private readonly ProjectRepository projectRepo;
+    private readonly ProjectPresetRepository presetRepo;
+    private readonly IDialogService dialog;
+
+    public CreateProjectFactory(
+        ProjectRepository projectRepo,
+        ProjectPresetRepository presetRepo,
+        IDialogService dialog)
+    {
+        this.projectRepo = projectRepo;
+        this.presetRepo = presetRepo;
+        this.dialog = dialog;
+    }
+
+    public CreateProjectViewModel Create(Project? existingProject = null)
+    {
+        var createProject = new CreateProjectViewModel(this.projectRepo, this.presetRepo, this.dialog, existingProject);
+        return createProject;
+    }
+}
---
+++ Phos.MusicManager.Library/ViewModels/Projects/Factories/ProjectFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Phos.MusicManager.Library.ViewModels.Projects.Factories;
+
+using Microsoft.Extensions.Logging;
+using Phos.MusicManager.Library.Audio;
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.Projects;
+using Phos.MusicManager.Library.ViewModels.Music;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class ProjectFactory
+{
+    private readonly AudioBuilder audioBuilder;
+    private readonly MusicFactory musicFactory;
+    private readonly CreateProjectFactory createProjectFactory;
+    private readonly IDialogService dialog;
+    private readonly ILogger? log;
+
+    public ProjectFactory(
+        AudioBuilder audioBuilder,
+        MusicFactory musicFactory,
+        CreateProjectFactory createProjectFactory,
+        IDialogService dialog,
+        ILogger? log = null)
+    {
+        this.audioBuilder = audioBuilder;
+        this.musicFactory = musicFactory;
+        this.createProjectFactory = createProjectFactory;
+        this.dialog = dialog;
+        this.log = log;
+    }
+
+    public ProjectViewModel Create(Project project)
+    {
+        var projectViewModel = new ProjectViewModel(project, this.audioBuilder, this.musicFactory, this.createProjectFactory, this.dialog, this.log);
+        return projectViewModel;
+    }
+}
