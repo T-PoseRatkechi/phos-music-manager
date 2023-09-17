ad263891d1541666e66c76d9b55546adaf2e5fca
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 06:20:01 -0500

    Added new create/edit project vm.

---
+++ Phos.MusicManager.Library/ViewModels/Projects/Dialogs/CreateProjectViewModel.cs
@@ -0,0 +1,102 @@
+﻿namespace Phos.MusicManager.Library.ViewModels.Projects.Dialogs;
+
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.Projects;
+using Phos.MusicManager.Library.ViewModels.Projects.Forms;
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1601 // Partial elements should be documented
+public partial class CreateProjectViewModel : WindowViewModelBase
+{
+    private readonly IDialogService dialog;
+
+    [ObservableProperty]
+    private string? iconFile;
+
+    public CreateProjectViewModel(
+        ProjectRepository projectRepo,
+        ProjectPresetRepository presetRepo,
+        IDialogService dialog,
+        Project? existingProject = null)
+    {
+        this.dialog = dialog;
+        this.Form = new(projectRepo, presetRepo, existingProject);
+        if (existingProject != null)
+        {
+            this.IsEditing = true;
+            this.IconFile = Path.Join(existingProject.ProjectFolder, "icon.png");
+        }
+
+        this.Form.ErrorsChanged += this.Form_ErrorsChanged;
+    }
+
+    public bool IsEditing { get; init; }
+
+    public CreateProjectForm Form { get; }
+
+    private bool CanConfirm => !this.Form.HasErrors;
+
+    [RelayCommand]
+    public override void Close(object? result = null)
+    {
+        this.Form.ErrorsChanged -= this.Form_ErrorsChanged;
+        base.Close(result);
+    }
+
+    [RelayCommand(CanExecute = nameof(this.CanConfirm))]
+    private void Confirm()
+    {
+        var projectSettings = new ProjectSettings
+        {
+            Name = this.Form.Name,
+            Preset = this.Form.SelectedPreset != CreateProjectForm.NoneOption ? this.Form.SelectedPreset : null,
+            Color = string.IsNullOrEmpty(this.Form.Color) ? null : this.Form.Color,
+            GameInstallPath = string.IsNullOrEmpty(this.Form.GameInstallPath) ? null : this.Form.GameInstallPath,
+            OutputDir = string.IsNullOrEmpty(this.Form.OutputDir) ? null : this.Form.OutputDir,
+            PostBuild = string.IsNullOrEmpty(this.Form.SelectedPostBuild) ? null : this.Form.SelectedPostBuild,
+        };
+
+        this.Close(projectSettings);
+    }
+
+    [RelayCommand]
+    private async Task SelectOutputFolder()
+    {
+        var outputDir = await this.dialog.OpenFolderSelect("Select Output Folder");
+        if (outputDir != null)
+        {
+            this.Form.OutputDir = outputDir;
+        }
+    }
+
+    [RelayCommand]
+    private async Task SelectGameInstall()
+    {
+        var gameInstallPath = await this.dialog.OpenFileSelect("Select Game Install");
+        if (gameInstallPath != null)
+        {
+            this.Form.GameInstallPath = gameInstallPath;
+        }
+    }
+
+    [RelayCommand]
+    private async Task SelectIcon()
+    {
+        var iconFile = await this.dialog.OpenFileSelect("Select Project Icon", "PNG files|*.png");
+        this.IconFile = iconFile;
+    }
+
+    [RelayCommand]
+    private void RemoveIcon()
+    {
+        this.IconFile = null;
+    }
+
+    private void Form_ErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
+    {
+        this.ConfirmCommand.NotifyCanExecuteChanged();
+    }
+}
---
+++ Phos.MusicManager.Library/ViewModels/Projects/Forms/CreateProjectForm.cs
@@ -0,0 +1,124 @@
+﻿namespace Phos.MusicManager.Library.ViewModels.Projects.Forms;
+
+using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Phos.MusicManager.Library.Projects;
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1601 // Partial elements should be documented
+public partial class CreateProjectForm : ObservableValidator
+{
+    public const string NoneOption = "None";
+    private readonly ProjectPresetRepository presetRepo;
+    private readonly ProjectRepository projectRepo;
+    private readonly Project? existingProject;
+
+    private string name = string.Empty;
+    private string selectedPreset = NoneOption;
+
+    [ObservableProperty]
+    private string color = string.Empty;
+
+    [ObservableProperty]
+    private string outputDir = string.Empty;
+
+    [ObservableProperty]
+    private string gameInstallPath = string.Empty;
+
+    public CreateProjectForm(
+        ProjectRepository projectRepo,
+        ProjectPresetRepository presetRepo,
+        Project? existingProject = null)
+    {
+        this.presetRepo = presetRepo;
+        this.projectRepo = projectRepo;
+        this.existingProject = existingProject;
+
+        this.PresetOptions = presetRepo.List.Select(x => x.Name).ToList();
+        this.PresetOptions.Insert(0, NoneOption);
+        this.PostBuildOptions = new();
+        this.PostBuildOptions.Insert(0, NoneOption);
+
+        if (existingProject != null)
+        {
+            this.Name = existingProject.Settings.Value.Name;
+            this.Color = existingProject.Settings.Value.Color ?? string.Empty;
+            this.SelectedPreset = existingProject.Settings.Value.Preset ?? NoneOption;
+            this.SelectedPostBuild = existingProject.Settings.Value.PostBuild ?? NoneOption;
+            this.GameInstallPath = existingProject.Settings.Value.GameInstallPath ?? string.Empty;
+            this.OutputDir = existingProject.Settings.Value.OutputDir ?? string.Empty;
+        }
+        else
+        {
+            this.SelectedPostBuild = NoneOption;
+        }
+
+        this.ValidateAllProperties();
+    }
+
+    [CustomValidation(typeof(CreateProjectForm), nameof(ValidateName))]
+    public string Name
+    {
+        get => this.name;
+        set => this.SetProperty(ref this.name, value, true);
+    }
+
+    public string SelectedPreset
+    {
+        get => this.selectedPreset;
+        set
+        {
+            this.SetProperty(ref this.selectedPreset, value);
+            this.SetPresetValues();
+        }
+    }
+
+    public List<string> PresetOptions { get; }
+
+    public string SelectedPostBuild { get; set; }
+
+    public List<string> PostBuildOptions { get; }
+
+    public static ValidationResult? ValidateName(string name, ValidationContext context)
+    {
+        var instance = (CreateProjectForm)context.ObjectInstance;
+
+        // Ignore duplicate name if editing project.
+        if (instance.existingProject != null && instance.Name == instance.existingProject.Settings.Value.Name)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (string.IsNullOrEmpty(name))
+        {
+            return new(null);
+        }
+
+        if (instance.projectRepo.List.FirstOrDefault(x => x.Settings.Value.Name == name) != null)
+        {
+            return new("Project with name already exists.");
+        }
+
+        return ValidationResult.Success;
+    }
+
+    private void SetPresetValues()
+    {
+        if (this.selectedPreset == NoneOption)
+        {
+            return;
+        }
+
+        var preset = this.presetRepo.GetById(this.selectedPreset);
+        if (preset != null)
+        {
+            // Only set color from preset if new project.
+            if (this.existingProject == null)
+            {
+                this.Color = preset.Color ?? string.Empty;
+            }
+
+            this.SelectedPostBuild = preset.PostBuild ?? string.Empty;
+        }
+    }
+}
