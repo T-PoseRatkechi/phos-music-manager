43ce3c34a66054cee3abd9dc7d5e822e85bcf73e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 14:40:03 -0500

    Added create preset command.

---
+++ Phos.MusicManager.Library/Commands/CreatePresetCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace Phos.MusicManager.Library.Commands;
+
+using System;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using Phos.MusicManager.Library.Projects;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class CreatePresetCommand
+{
+    private readonly ILogger? log;
+    private readonly ProjectPresetRepository presetRepository;
+
+    public CreatePresetCommand(ProjectPresetRepository presetRepository, ILogger? log = null)
+    {
+        this.log = log;
+        this.presetRepository = presetRepository;
+    }
+
+    public IRelayCommand Create(Project project, Func<bool>? canExecute = null) =>
+        canExecute == null ? new RelayCommand(() => this.CreatePreset(project)) : new RelayCommand(() => this.CreatePreset(project), canExecute);
+
+    private void CreatePreset(Project project)
+    {
+        try
+        {
+            this.presetRepository.Create(project);
+        }
+        catch (Exception ex)
+        {
+            this.log?.LogError(ex, "Failed to create preset from project {project}.", project.Settings.Value.Name);
+        }
+    }
+}
