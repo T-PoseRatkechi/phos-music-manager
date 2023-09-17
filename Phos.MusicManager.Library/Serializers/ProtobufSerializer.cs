cd83cd92e5a1dfb34b2ffaeb9b35347aebfdf782
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 06:15:29 -0500

    Added protobuf serializer.

---
+++ Phos.MusicManager.Library/Serializers/ProtobufSerializer.cs
@@ -0,0 +1,26 @@
+﻿namespace Phos.MusicManager.Library.Serializers;
+
+using ProtoBuf;
+
+#pragma warning disable SA1600 // Elements should be documented
+public static class ProtobufSerializer
+{
+    public static void Serialize(string file, object? value)
+    {
+        var dir = Path.GetDirectoryName(file);
+        if (dir != null)
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        using var fs = File.Create(file);
+        Serializer.Serialize(fs, value);
+    }
+
+    public static T? Deserialize<T>(string file)
+    {
+        using var fs = File.OpenRead(file);
+        var value = Serializer.Deserialize<T>(fs);
+        return value;
+    }
+}
