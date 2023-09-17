3acb2a46c4342787e6c1a22c289c3e8cfbfb98ba
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 16:59:02 -0500

    Added converter to get correct nav item name.

---
+++ Phos.MusicManager.Desktop/Converters/ProjectNavigationItemConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Data.Converters;
+using Phos.MusicManager.Library.Navigation;
+using Phos.MusicManager.Library.ViewModels;
+using System;
+using System.Globalization;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+public class ProjectNavigationItemConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is ProjectViewModel projectPage)
+        {
+            return projectPage.Project.Settings.Value.Name;
+        }
+        else if (value is IPage page)
+        {
+            return page.Name;
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
