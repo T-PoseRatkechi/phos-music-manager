1b4cd79d6560eb26c3c7fa9ca79e2787647607e5
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-18 13:21:43 -0500

    Added project type.

---
+++ Phos.MusicManager.Library/Projects/ProjectType.cs
@@ -0,0 +1,10 @@
+﻿namespace Phos.MusicManager.Library.Projects;
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1602 // Enumeration items should be documented
+public enum ProjectType
+{
+    Version1,
+
+    Portable1,
+}
