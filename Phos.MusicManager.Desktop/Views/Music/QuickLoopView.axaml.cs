ec75dcf5835b6a83b40b6adf4cfbb94ebf8f3418
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-18 11:08:23 -0500

    Added quick loop view.

---
+++ Phos.MusicManager.Desktop/Views/Music/QuickLoopView.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Markup.Xaml;
+using Phos.MusicManager.Desktop.Controls;
+
+namespace Phos.MusicManager.Desktop.Views.Music;
+
+public partial class QuickLoopView : AppWindow
+{
+    public QuickLoopView()
+    {
+        InitializeComponent();
+    }
+}
