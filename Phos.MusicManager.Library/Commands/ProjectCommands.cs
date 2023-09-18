2d2134b94f060666f443030e85900d6a5d2ba54d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-18 16:58:00 -0500

    Added open project command.

--- Phos.MusicManager.Library/Commands/ProjectCommands.cs
+++ Phos.MusicManager.Library/Commands/ProjectCommands.cs
@@ -5,6 +5,7 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using Phos.MusicManager.Library.Common;
 using Phos.MusicManager.Library.Projects;
+using Phos.MusicManager.Library.ViewModels.Dialogs;
 using Phos.MusicManager.Library.ViewModels.Projects.Factories;
 
 #pragma warning disable SA1600 // Elements should be documented
@@ -12,25 +13,26 @@ using Phos.MusicManager.Library.ViewModels.Projects.Factories;
 public partial class ProjectCommands : ObservableObject
 {
     private readonly ProjectRepository projectRepository;
+    private readonly PortableProjectConverter projectConverter;
     private readonly CreateProjectFactory createProjectFactory;
     private readonly IDialogService dialog;
     private readonly ILogger? log;
 
     public ProjectCommands(
         ProjectRepository projectRepository,
+        PortableProjectConverter projectConverter,
         CreateProjectFactory createProjectFactory,
         IDialogService dialog,
         ILogger? log = null)
     {
         this.projectRepository = projectRepository;
+        this.projectConverter = projectConverter;
         this.createProjectFactory = createProjectFactory;
         this.dialog = dialog;
         this.log = log;
     }
 
-    public IRelayCommand Create_NewProjectCommand(Func<bool>? canExecute = null)
-        => canExecute == null ? new AsyncRelayCommand(this.NewProject) : new AsyncRelayCommand(this.NewProject, canExecute);
-
+    [RelayCommand]
     private async Task NewProject()
     {
         var createProject = this.createProjectFactory.Create();
@@ -53,6 +55,47 @@ public partial class ProjectCommands : ObservableObject
         catch (Exception ex)
         {
             this.log?.LogError(ex, "Failed to create new project.");
+        }
+    }
+
+    [RelayCommand]
+    private async Task OpenProject()
+    {
+        var projectFile = await this.dialog.OpenFileSelect("Open Project", "Project File|*.phos");
+        if (projectFile == null)
+        {
+            return;
+        }
+
+        try
+        {
+            var project = new Project(projectFile);
+            if (project.Settings.Value.Type == ProjectType.Portable1)
+            {
+                var confirmDialog = new ConfirmViewModel()
+                {
+                    Title = "Open Project",
+                    Subtitle = "Convert Portable Project",
+                    BodyText = "Opening the portable project will convert it to a normal project. Continue?",
+                    ConfirmText = "Convert Project",
+                    CancelText = "Cancel",
+                };
+
+                var confirmConvert = await this.dialog.OpenDialog<bool>(confirmDialog);
+                if (!confirmConvert)
+                {
+                    return;
+                }
+
+                // Convert project.
+                this.projectConverter.Convert(project);
+            }
+
+            this.projectRepository.Add(project);
+        }
+        catch (Exception ex)
+        {
+            this.log?.LogError(ex, "Failed to open project.");
         }
     }
 }
