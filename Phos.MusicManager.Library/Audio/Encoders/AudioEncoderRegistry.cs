9754a858148b6cd9475a981b9a7dc30ab82d9c89
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-17 22:58:46 -0500

    Updated encoder registry to use vgaudio encoders.

--- Phos.MusicManager.Library/Audio/Encoders/AudioEncoderRegistry.cs
+++ Phos.MusicManager.Library/Audio/Encoders/AudioEncoderRegistry.cs
@@ -1,14 +1,13 @@
 ﻿namespace Phos.MusicManager.Library.Audio.Encoders;
 
+using IniParser;
 using Microsoft.Extensions.Logging;
-using Phos.MusicManager.Library.Audio.Models;
-using Phos.MusicManager.Library.Common.Serializers;
-using VGAudio.Containers.Adx;
-using VGAudio.Containers.Hca;
+using Phos.MusicManager.Library.Audio.Encoders.VgAudio;
+using VGAudio.Codecs.CriAdx;
+using VGAudio.Codecs.CriHca;
+using VGAudio.Utilities;
 
-/// <summary>
-/// Audio encoder registry.
-/// </summary>
+#pragma warning disable SA1600 // Elements should be documented
 public class AudioEncoderRegistry
 {
     private readonly string encodersDir = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "audio", "encoders");
@@ -25,67 +24,66 @@ public class AudioEncoderRegistry
         Directory.CreateDirectory(this.encodersDir);
         Directory.CreateDirectory(this.cacheDir);
 
-        this.Encoders = this.GetEncoders();
+        this.LoadVgAudioEncoders();
     }
 
-    /// <summary>
-    /// Gets available encoders.
-    /// </summary>
-    public Dictionary<string, IEncoder> Encoders { get; }
+    public Dictionary<string, IEncoder> Encoders { get; } = new();
 
-    private Dictionary<string, IEncoder> GetEncoders()
+    public void LoadVgAudioEncoders()
     {
-        var encoders = new Dictionary<string, IEncoder>()
-        {
-            { "HCA", this.CreateCachedEncoder(new HcaEncoder(this.log), "hca") },
-            { "ADX", this.CreateCachedEncoder(new AdxEncoder(this.log), "adx") },
-        };
+        var vgAudioDir = Directory.CreateDirectory(Path.Join(this.encodersDir, "vgaudio")).FullName;
 
-        foreach (var configFile in Directory.EnumerateFiles(this.encodersDir, "*.json", SearchOption.AllDirectories))
+        // Add default encoders.
+        foreach (var container in ContainerTypes.Containers)
         {
             try
             {
-                var config = JsonFileSerializer.Deserialize<EncoderConfig>(configFile)!;
-                if (encoders.ContainsKey(config.Name))
-                {
-                    this.log?.LogWarning("Duplicate encoder with name {name}.\nFile: {file}", config.Name, configFile);
-                    continue;
-                }
-
-                var encoder = this.CreateCachedEncoder(this.CreateEncoder(config), Path.GetFileNameWithoutExtension(configFile));
-                encoders.Add(config.Name, encoder);
+                var type = container.Value;
+                var name = type.Names.First();
+                var encoder = new VgAudioEncoder(new() { OutContainerFormat = name });
+                this.Encoders.Add(name.ToUpper(), this.CreateCachedEncoder(encoder, name));
             }
             catch (Exception ex)
             {
-                this.log?.LogWarning(ex, "Failed to create encoder with config.\nFile: {file}", configFile);
+                this.log?.LogError(ex, "Failed to create default encoder.");
             }
         }
 
-        return encoders;
-    }
-
-    private IEncoder CreateEncoder(EncoderConfig config)
-    {
-        switch (config.Encoder)
+        // Add from ini configs.
+        var parser = new FileIniDataParser();
+        foreach (var file in Directory.EnumerateFiles(vgAudioDir, "*.ini", SearchOption.AllDirectories))
         {
-            case "HCA":
-                var hcaConfig = new HcaConfiguration
+            try
+            {
+                var data = parser.ReadFile(file);
+                var config = new Config
                 {
-                    Bitrate = config.Bitrate,
-                    EncryptionKey = new(config.EncryptionKey),
-                    Quality = config.Quality.ToHcaQuality(),
+                    Name = data.GetKey("name") ?? throw new Exception("Name is missing."),
+                    OutContainerFormat = data.GetKey("out_container_format")?.ToUpper() ?? throw new Exception("Missing output container format."),
+                    OutFormat = data.GetKey("out_format")?.ToUpper(),
+                    LoopAlignment = int.TryParse(data.GetKey("loop_alignment"), out var loopAlignment) ? loopAlignment : default,
+                    BlockSize = int.TryParse(data.GetKey("block_size"), out var blockSize) ? blockSize : default,
+                    Version = int.TryParse(data.GetKey("version"), out var version) ? version : default,
+                    FrameSize = int.TryParse(data.GetKey("frame_size"), out var frameSize) ? frameSize : default,
+                    Filter = int.TryParse(data.GetKey("filter"), out var filter) ? filter : 2,
+                    AdxType = Enum.TryParse<CriAdxType>(data.GetKey("adx_type"), out var adxType) ? adxType : default,
+                    KeyString = data.GetKey("key_string"),
+                    KeyCode = ulong.TryParse(data.GetKey("key_code"), out var keyCode) ? keyCode : default,
+                    Endianness = Enum.TryParse<Endianness>(data.GetKey("endianess"), out var endianess) ? endianess : default,
+                    HcaQuality = Enum.TryParse<CriHcaQuality>(data.GetKey("hca_quality"), out var hcaQuality) ? hcaQuality : default,
+                    Bitrate = int.TryParse(data.GetKey("bitrate"), out var bitrate) ? bitrate : default,
+                    LimitBitrate = bool.TryParse(data.GetKey("limit_bitrate"), out var limitBitrate) && limitBitrate,
+                    EncodeCbr = bool.TryParse(data.GetKey("encode_cbr"), out var encodeCbr) && encodeCbr,
                 };
 
-                return new HcaEncoder(hcaConfig, this.log);
-            case "ADX":
-                var adxConfig = new AdxConfiguration
-                {
-                    EncryptionKey = new(config.EncryptionKey),
-                    EncryptionType = config.EncryptionType,
-                };
-
-                return new AdxEncoder(adxConfig, this.log);
-            default: throw new ArgumentException($"Unknown encoder: {config.Encoder}");
+                var encoder = new VgAudioEncoder(config);
+                var cachedName = Path.GetFileNameWithoutExtension(file);
+                this.Encoders.Add(config.Name, this.CreateCachedEncoder(encoder, cachedName));
+            }
+            catch (Exception ex)
+            {
+                this.log?.LogError(ex, "Failed to create encoder from file.\nFile: {file}", file);
+            }
         }
     }
 
