50dfed29e1f4733fd4443d0b3de0d946cd85a91c
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-18 16:45:35 -0500

    Added confirm dialog.

---
+++ Phos.MusicManager.Desktop/Views/Dialogs/ConfirmView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Phos.MusicManager.Desktop.Controls;
+
+namespace Phos.MusicManager.Desktop.Views.Dialogs;
+
+public partial class ConfirmView : AppWindow
+{
+    public ConfirmView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Phos.MusicManager.Library/ViewModels/Dialogs/ConfirmViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Phos.MusicManager.Library.ViewModels.Dialogs;
+
+using CommunityToolkit.Mvvm.Input;
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1601 // Partial elements should be documented
+public partial class ConfirmViewModel : WindowViewModelBase
+{
+    public string Title { get; set; } = "Confirm";
+
+    public string? Subtitle { get; set; }
+
+    public string? BodyText { get; set; }
+
+    public string ConfirmText { get; set; } = "Confirm";
+
+    public string CancelText { get; set; } = "Cancel";
+
+    [RelayCommand]
+    public override void Close(object? result = null)
+    {
+        base.Close(result ?? false);
+    }
+
+    [RelayCommand]
+    private void Confirm()
+    {
+        this.Close(true);
+    }
+}
