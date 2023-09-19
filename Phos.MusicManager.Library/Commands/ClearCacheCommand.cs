2c335630968e6ac43be4672f736f82da25a262eb
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-18 20:40:35 -0500

    Created cache and refresh commands.

---
+++ Phos.MusicManager.Library/Commands/ClearCacheCommand.cs
@@ -0,0 +1,51 @@
+﻿namespace Phos.MusicManager.Library.Commands;
+
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using Phos.MusicManager.Library.Common;
+using Phos.MusicManager.Library.ViewModels.Dialogs;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class ClearCacheCommand
+{
+    private readonly ILogger? log;
+    private readonly IDialogService dialog;
+
+    public ClearCacheCommand(IDialogService dialog, ILogger? log = null)
+    {
+        this.log = log;
+        this.dialog = dialog;
+    }
+
+    public IRelayCommand Create() =>
+        new AsyncRelayCommand(this.ClearCache);
+
+    private async Task ClearCache()
+    {
+        var cachedDir = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "audio", "cached");
+        var confirmClearDialog = new ConfirmViewModel()
+        {
+            Title = "Clear Cache",
+            Subtitle = cachedDir,
+            BodyText = "Clear all files in cached folder?",
+        };
+
+        var confirmClear = await this.dialog.OpenDialog<bool>(confirmClearDialog);
+        if (!confirmClear)
+        {
+            return;
+        }
+
+        try
+        {
+            foreach (var file in Directory.EnumerateFiles(cachedDir, "*.*", SearchOption.AllDirectories))
+            {
+                File.Delete(file);
+            }
+        }
+        catch (Exception ex)
+        {
+            this.log?.LogError(ex, "Failed to clear cache.");
+        }
+    }
+}
---
+++ Phos.MusicManager.Library/Commands/RefreshFilesCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace Phos.MusicManager.Library.Commands;
+
+using System;
+using System.Windows.Input;
+using Phos.MusicManager.Library.Audio.Encoders;
+using Phos.MusicManager.Library.Projects;
+
+#pragma warning disable SA1600 // Elements should be documented
+public class RefreshFilesCommand : ICommand
+{
+    private readonly AudioEncoderRegistry encoderRegistry;
+    private readonly ProjectPresetRepository presetRepository;
+
+    public RefreshFilesCommand(AudioEncoderRegistry encoderRegistry, ProjectPresetRepository presetRepository)
+    {
+        this.encoderRegistry = encoderRegistry;
+        this.presetRepository = presetRepository;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        this.encoderRegistry.LoadEncoders();
+        this.presetRepository.LoadPresets();
+    }
+}
