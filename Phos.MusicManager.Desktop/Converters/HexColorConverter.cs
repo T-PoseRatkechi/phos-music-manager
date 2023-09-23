e9ec40e718d967b538fc6b1c4f03cfd0c8ab6df7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-23 18:30:18 -0500

    Added project color converter.

---
+++ Phos.MusicManager.Desktop/Converters/HexColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+public class HexColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        try
+        {
+            if (value is string color)
+            {
+                return Brush.Parse(color);
+            }
+            else
+            {
+                return Brush.Parse("Turquoise");
+            }
+        }
+        catch (Exception)
+        {
+            return Brush.Parse("Turquoise");
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
