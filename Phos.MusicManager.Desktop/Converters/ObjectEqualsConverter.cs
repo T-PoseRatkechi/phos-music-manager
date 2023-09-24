cd42c83c9502a69292803f1bc922e1e043a48896
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-24 08:06:53 -0500

    Added equality converter.

---
+++ Phos.MusicManager.Desktop/Converters/ObjectEqualsConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Phos.MusicManager.Desktop.Converters;
+
+public class ObjectEqualsConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value?.Equals(parameter) ?? false)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
