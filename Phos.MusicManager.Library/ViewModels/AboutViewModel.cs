97d612d17a133f4c87c8ee8d3d225023e4447654
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-24 16:34:07 -0500

    Added credit.

--- Phos.MusicManager.Library/ViewModels/AboutViewModel.cs
+++ Phos.MusicManager.Library/ViewModels/AboutViewModel.cs
@@ -11,10 +11,11 @@ public class AboutViewModel : ViewModelBase, IPage
     public Credit[] Credits { get; } = new Credit[]
     {
         new("T-PoseRatkechi", "Creator"),
-        new("Thealexbarney", "VGAudio library, core audio encoder.", "https://github.com/Thealexbarney/VGAudio"),
+        new("Thealexbarney", "VGAudio, core audio encoder.", "https://github.com/Thealexbarney/VGAudio"),
         new("Pixelguin", "Provided Persona 4 Golden 64-bit music data and song categories.", "https://gamebanana.com/members/1736439"),
         new("aray03", "Source for Persona 5 Royal PC music data.", "https://gamebanana.com/members/2323791"),
         new("ARandomGuy231 + Various Modders", "Source for Persona 3 Portable music data.", "https://gamebanana.com/members/1742788"),
+        new("Lorc, Delapouite & contributors", "App icon. CC BY 3.0.", "https://game-icons.net"),
     };
 
     public record Credit(string Name, string Description, string? Url = null);
