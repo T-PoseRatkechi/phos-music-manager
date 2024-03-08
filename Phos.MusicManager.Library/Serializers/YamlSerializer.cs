071ea3119f4e4ac925dad4ee07addbfdeb5cf2c9
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-03-08 02:36:45 -0600

    Add YAML serializer.

---
+++ Phos.MusicManager.Library/Serializers/YamlSerializer.cs
@@ -0,0 +1,34 @@
+﻿namespace Phos.MusicManager.Library.Serializers;
+
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+#pragma warning disable SA1600 // Elements should be documented
+public static class YamlSerializer
+{
+    private static readonly ISerializer Serializer = new SerializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .DisableAliases()
+            .Build();
+
+    private static readonly IDeserializer Deserializer = new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+
+    public static void Serialize(string file, object? value)
+    {
+        var dir = Path.GetDirectoryName(file);
+        if (dir != null)
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        File.WriteAllText(file, Serializer.Serialize(value));
+    }
+
+    public static T? Deserialize<T>(string file)
+    {
+        var value = Deserializer.Deserialize<T>(File.ReadAllText(file));
+        return value;
+    }
+}
